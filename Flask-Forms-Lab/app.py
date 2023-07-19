5af5e11e90b07afd79511748e65efc53ac8bb15a
alonaly24-meet <alona.lynfield@gmail.com>
2023-07-19 12:14:38 +0300

    nn

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -7,18 +7,25 @@ app = Flask(  # Create a flask app
 	static_folder='static'  # Name of directory for static files
 )
 
-
 username = "llo2ay"
 password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+facebook_friends=["malak","rut","nada", "talyah", "and more"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
+	if request.method=="POST":
+		if username==request.form.get("username") and password==request.form.get("password"):
+			return redirect(url_for('home'))
+		else:
+			return render_template("login.html")
+	else:  
+		return render_template('login.html')
+
+@app.route("/home",methods=["GET","POST"])
+def home():
+	return render_template('home.html',friends=facebook_friends)
   
-
-
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
